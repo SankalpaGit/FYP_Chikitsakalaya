0c86a0f23b1f331b13f76d8d395a22d6c2ef35fa
Sankalpa <joshisankalpa2@gmail.com>
2025-02-15 17:54:06 +0545

    dynamic title component added

---
+++ frontend/src/pages/TitleChanger.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const TitleChanger = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    let title = "Chikitsakalaya"; // Default title
+
+    // Change title based on the route
+    switch (location.pathname) {
+      case "/login":
+        title = "login - Chikitsakalaya";
+        break;
+      case "/profile":
+        title = "Profile";
+        break;
+      case "/report":
+        title = "Reports - Chikitsakalaya";
+        break;
+      case "/prescription":
+        title = "Prescription - Chikitsakalaya";
+        break;
+      default:
+        title = "Chikitsakalaya";
+        break;
+    }
+
+    document.title = title; // Update the document title
+  }, [location]);
+
+  return null; // This component does not render anything
+};
+
+export default TitleChanger;
--- frontend/src/pages/patients/LoginForm.jsx
+++ frontend/src/pages/patients/LoginForm.jsx
@@ -19,7 +19,7 @@ function LoginForm() {
       setMessageColor('text-green-600');
       setTimeout(() => {
         setMessage('');
-        navigate('/home');
+        navigate('/');
       }, 2000);
     } catch (error) {
       console.error('Login error:', error);
