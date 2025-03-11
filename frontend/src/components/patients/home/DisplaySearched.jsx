4bd4332f8cdf0c9ac8c9bab02a0f4193f23b2cbd
Sankalpa <joshisankalpa2@gmail.com>
2025-03-11 19:12:25 +0545

    search array content started to siaplyed

--- frontend/src/components/patients/home/DisplaySearched.jsx
+++ frontend/src/components/patients/home/DisplaySearched.jsx
@@ -4,7 +4,6 @@ import { CiHospital1 } from "react-icons/ci";
 import { GrUserExpert } from "react-icons/gr";
 import { useNavigate } from "react-router-dom";
 
-
 const DisplaySearched = ({ searchResults }) => {
     const [activeDoctor, setActiveDoctor] = useState(null);
     const [activeTab, setActiveTab] = useState(null);
@@ -14,143 +13,152 @@ const DisplaySearched = ({ searchResults }) => {
 
     const toggleTab = (doctorId, tab) => {
         if (activeDoctor === doctorId && activeTab === tab) {
-            // If clicking the same tab again, close everything
             setActiveDoctor(null);
             setActiveTab(null);
         } else {
-            // Open the section and set the active tab
             setActiveDoctor(doctorId);
             setActiveTab(tab);
         }
     };
-    
+
+    // Function to check if the image URL is valid
+    const isValidImageUrl = (url) => {
+        return url && (url.startsWith('http') || url.startsWith('https'));
+    };
 
     return (
         <div className='w-8/12 bg-white shadow-lg border-2 rounded-2xl border-gray-300 px-6 py-6 h-fit'>
             {searchResults?.length > 0 ? (
-                searchResults.map((doctor) => (
-                    <div key={doctor.id} className="mb-6">
-                        <div className='w-full flex items-start'>
-                            <div className='w-3/12 h-38 object-cover'>
-                                <img
-                                    src={doctor.doctorDetails.profilePicture || defaultProfilePic}
-                                    alt="Doctor Avatar"
-                                    className='w-full h-full rounded-lg'
-                                />
-                            </div>
-                            <div className='w-6/12 p-5'>
-                                <p className='font-bold text-gray-700 text-xl'>
-                                    {doctor.firstName} {doctor.lastName}
-                                </p>
-                                <p className='text-gray-700 font-semibold'>{doctor.doctorDetails.speciality}</p>
-                                <div className='flex items-center mt-5'>
-                                    <p className='text-orange-600 font-bold text-xl'>Rs {doctor.doctorDetails.consultationFee}</p>
-                                    <span className='ml-2 font-semibold text-gray-500'>per hour</span>
-                                </div>
-                                <div className='flex items-center mt-8 text-gray-700'>
-                                    <FaHospital className='text-orange-600 mr-2 text-2xl' />
-                                    <p>{doctor.doctorDetails.hospitalAffiliation}</p>
-                                </div>
-                            </div>
-                            <div className='w-2/12 flex flex-col items-center h-44'>
-                            <button
-                                    className='bg-orange-600 py-3 px-3 text-white rounded-md mt-6'
-                                    onClick={() => navigate(`/appointment/${doctor.id}`)}
-                                >
-                                    Book Now
-                                </button>
-                                <p
-                                    className='text-orange-500 text-lg font-semibold underline mt-auto cursor-pointer'
-                                    onClick={() => toggleTab(doctor.id, 'details')}
-                                >
-                                    View More
-                                </p>
-                            </div>
-                        </div>
+                searchResults.map((doctor) => {
+                    const doctorDetails = doctor.doctorDetails?.[0] || {};  // Access the first element of doctorDetails array
+                    const profilePicUrl = doctorDetails.profilePicture
+                        ? `http://localhost:5000/${doctorDetails.profilePicture.replace(/\\/g, '/')}` 
+                        : defaultProfilePic;
 
-                        {/* below is Expandable Details */}
-                        {activeDoctor === doctor.id && (
-                            <div className="h-fit bg-white w-full p-5">
-                                <div className="h-0.5 w-full bg-gray-300 my-5"></div>
-                                <div className="flex space-x-8 mb-5">
+                    return (
+                        <div key={doctor.id} className="mb-6">
+                            <div className='w-full flex items-start'>
+                                <div className='w-3/12 h-38 object-cover'>
+                                    <img
+                                        src={profilePicUrl}
+                                        alt="Doctor Avatar"
+                                        className='w-full h-full rounded-lg'
+                                    />
+                                </div>
+                                <div className='w-6/12 p-5'>
+                                    <p className='font-bold text-gray-700 text-xl'>
+                                        {doctor.firstName} {doctor.lastName}
+                                    </p>
+                                    <p className='text-gray-700 font-semibold'>{doctorDetails.speciality}</p>
+                                    <div className='flex items-center mt-5'>
+                                        <p className='text-orange-600 font-bold text-xl'>Rs {doctorDetails.consultationFee}</p>
+                                        <span className='ml-2 font-semibold text-gray-500'>per hour</span>
+                                    </div>
+                                    <div className='flex items-center mt-8 text-gray-700'>
+                                        <FaHospital className='text-orange-600 mr-2 text-2xl' />
+                                        <p>{doctorDetails.hospitalAffiliation}</p>
+                                    </div>
+                                </div>
+                                <div className='w-2/12 flex flex-col items-center h-44'>
+                                    <button
+                                        className='bg-orange-600 py-3 px-3 text-white rounded-md mt-6'
+                                        onClick={() => navigate(`/appointment/${doctor.id}`)}
+                                    >
+                                        Book Now
+                                    </button>
                                     <p
-                                        className={`flex items-center cursor-pointer ${activeTab === 'details' ? 'font-bold text-orange-600' : 'text-gray-600'}`}
+                                        className='text-orange-500 text-lg font-semibold underline mt-auto cursor-pointer'
                                         onClick={() => toggleTab(doctor.id, 'details')}
                                     >
-                                        <FaUserMd className="mr-2" /> Doctor Details
-                                    </p>
-                                    <p
-                                        className={`flex items-center cursor-pointer ${activeTab === 'rules' ? 'font-bold text-orange-600' : 'text-gray-600'}`}
-                                        onClick={() => toggleTab(doctor.id, 'rules')}
-                                    >
-                                        <FaClipboardList className="mr-2" /> Appointment Rules
+                                        View More
                                     </p>
                                 </div>
-
-                                {activeTab === 'details' && (
-                                    <div className="space-y-3">
-                                        <div className="flex items-center">
-                                            <FaAddressCard className="text-xl text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">NMC number :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.licenseNumber}</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <GrUserExpert className="text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">Years of Experience :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.doctorDetails.experience} years</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <FaEnvelope className="text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">Email :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.email}</p>
-                                        </div>
-                                        <div className="h-0.5 w-full bg-gray-300"></div>
-                                        <div className="flex items-center">
-                                            <CiHospital1 className="text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">Hospital Address :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.doctorDetails.address}</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <FaMapMarkerAlt className="text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">District, Province :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.doctorDetails.city}, {doctor.doctorDetails.state}</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <FaFlag className="text-gray-600 mr-3" />
-                                            <p className="font-semibold text-gray-700">Country :</p>
-                                            <p className="text-gray-500 ml-3">{doctor.doctorDetails.country}</p>
-                                        </div>
-                                    </div>
-                                )}
-
-                                {activeTab === 'rules' && (
-                                    <div className="space-y-3">
-                                        <div className="flex items-center">
-                                            <strong className="text-4xl text-teal-700 mr-3">•</strong>
-                                            <p className="text-gray-700">Cannot cancel the appointment on the appointment day</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <strong className="text-4xl text-teal-700 mr-3">•</strong>
-                                            <p className="text-gray-700">The fee shown is valid only for one hour</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <strong className="text-4xl text-teal-700 mr-3">•</strong>
-                                            <p className="text-gray-700">In case of extra time, additional charges apply</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <strong className="text-4xl text-teal-700 mr-3">•</strong>
-                                            <p className="text-gray-700">For physical consultations, a digital token is mandatory</p>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <strong className="text-4xl text-teal-700 mr-3">•</strong>
-                                            <p className="text-gray-700">No refunds for missed appointments</p>
-                                        </div>
-                                    </div>
-                                )}
                             </div>
-                        )}
-                    </div>
-                ))
+
+                            {/* Expandable Details */}
+                            {activeDoctor === doctor.id && (
+                                <div className="h-fit bg-white w-full p-5">
+                                    <div className="h-0.5 w-full bg-gray-300 my-5"></div>
+                                    <div className="flex space-x-8 mb-5">
+                                        <p
+                                            className={`flex items-center cursor-pointer ${activeTab === 'details' ? 'font-bold text-orange-600' : 'text-gray-600'}`}
+                                            onClick={() => toggleTab(doctor.id, 'details')}
+                                        >
+                                            <FaUserMd className="mr-2" /> Doctor Details
+                                        </p>
+                                        <p
+                                            className={`flex items-center cursor-pointer ${activeTab === 'rules' ? 'font-bold text-orange-600' : 'text-gray-600'}`}
+                                            onClick={() => toggleTab(doctor.id, 'rules')}
+                                        >
+                                            <FaClipboardList className="mr-2" /> Appointment Rules
+                                        </p>
+                                    </div>
+
+                                    {activeTab === 'details' && (
+                                        <div className="space-y-3">
+                                            <div className="flex items-center">
+                                                <FaAddressCard className="text-xl text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">NMC number :</p>
+                                                <p className="text-gray-500 ml-3">{doctor.licenseNumber}</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <GrUserExpert className="text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">Years of Experience :</p>
+                                                <p className="text-gray-500 ml-3">{doctorDetails.experience} years</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <FaEnvelope className="text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">Email :</p>
+                                                <p className="text-gray-500 ml-3">{doctor.email}</p>
+                                            </div>
+                                            <div className="h-0.5 w-full bg-gray-300"></div>
+                                            <div className="flex items-center">
+                                                <CiHospital1 className="text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">Hospital Address :</p>
+                                                <p className="text-gray-500 ml-3">{doctorDetails.address}</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <FaMapMarkerAlt className="text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">District, Province :</p>
+                                                <p className="text-gray-500 ml-3">{doctorDetails.city}, {doctorDetails.state}</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <FaFlag className="text-gray-600 mr-3" />
+                                                <p className="font-semibold text-gray-700">Country :</p>
+                                                <p className="text-gray-500 ml-3">{doctorDetails.country}</p>
+                                            </div>
+                                        </div>
+                                    )}
+
+                                    {activeTab === 'rules' && (
+                                        <div className="space-y-3">
+                                            <div className="flex items-center">
+                                                <strong className="text-4xl text-teal-700 mr-3">•</strong>
+                                                <p className="text-gray-700">Cannot cancel the appointment on the appointment day</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <strong className="text-4xl text-teal-700 mr-3">•</strong>
+                                                <p className="text-gray-700">The fee shown is valid only for one hour</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <strong className="text-4xl text-teal-700 mr-3">•</strong>
+                                                <p className="text-gray-700">In case of extra time, additional charges apply</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <strong className="text-4xl text-teal-700 mr-3">•</strong>
+                                                <p className="text-gray-700">For physical consultations, a digital token is mandatory</p>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <strong className="text-4xl text-teal-700 mr-3">•</strong>
+                                                <p className="text-gray-700">No refunds for missed appointments</p>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    );
+                })
             ) : (
                 <p className="text-red-500 text-center w-full font-semibold">No doctors found. Please try again.</p>
             )}
